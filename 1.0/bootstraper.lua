31d6a2e8c100d4b507664f937a121a6d9a94fd13
aidarkunakbaev2 <kunakbaevaidar26@gmail.com>
2026-05-14 13:20:13 +0500

    Create bootstraper.lua

---
+++ 1.0/bootstraper.lua
@@ -0,0 +1,12 @@
+--[[
+ __      __                     .__                
+/  \    /  \_____ _______  ____ |__| ____    ____  
+\   \/\/   /\__  \\_  __ \/    \|  |/    \  / ___\ 
+ \        /  / __ \|  | \/   |  \  |   |  \/ /_/  >
+  \__/\  /  (____  /__|  |___|  /__|___|  /\___  / 
+       \/        \/           \/        \//_____/  
+This is outdated version. It have 12 only functions AND NO BYPASS. YOU WILL GET 100% BAN (NOT IN ROBLOX BUT IN GAME)
+Use latest version if you want more functions and bypasses 
+--]]
+
+--DELETED
